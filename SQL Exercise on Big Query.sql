1f290a10afa584692447391e967dffdf7952f41a
Sharaar <walidsharar@gmail.com>
2022-04-27 13:20:27 +0200

    Question 2

--- SQL Exercise on Big Query.sql
+++ SQL Exercise on Big Query.sql
@@ -3,3 +3,6 @@
 SELECT Gender, Geography, sum(NumOfProducts)
 from `big-query-348313.Churn.Churn`
 group by 1,2;
+
+-- Write a query which compares all the active and inactive users who have a credit card?
+
